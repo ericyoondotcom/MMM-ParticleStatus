440807f2d9fe868f8a099946d4606c1ac7897f2b
Eric <emjyoon@gmail.com>
2018-07-16 18:51:24 -0700

    Use () => notation to fix this conflict

--- MMM-ParticleStatus.js
+++ MMM-ParticleStatus.js
@@ -36,7 +36,6 @@ Module.register("MMM-ParticleStatus",{
     
     start: function() {
         var particle = new Particle();
-        var config = this.config; //needed because inside the particle.login.then, this.config gives config for whole MM!
         particle.login({username: this.config.particleUsername, password: this.config.particlePassword}).then(
           function(data) {
             var token = data.body.access_token;
@@ -46,11 +45,11 @@ Module.register("MMM-ParticleStatus",{
                     this.updateDom();
                 }
             }, 1000);
-            for(var i = 0; i < config.events.length; i++){
-                var event = config.events[i];
+            for(var i = 0; i < this.config.events.length; i++){
+                var event = this.config.events[i];
                 this.state.push("off");
-                particle.getEventStream({ deviceId: event.deviceId, auth: token }).then(function(stream) {
-                  stream.on(event.name, function(data) {
+                particle.getEventStream({ deviceId: event.deviceId, auth: token }).then((stream) => {
+                  stream.on(event.name, (data) => {
                     var newState = event.states[data.name];
                     if(newState != undefined || (newState != "off" && newState != "on" && newState != "blink"))
                         this.state[i] = newState;
